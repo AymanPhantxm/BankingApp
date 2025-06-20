a22dcebef4bdf0c42591ad5c1ee0a69302edd3b7
Ayman Yaqoub-Okponobi <aymanokponobi13@gmail.com>
2025-06-20 11:07:40 +0100

    blah

---
+++ BankingApp/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class User
+{
+    public string Username { get; }
+    public string Password { get; }
+    public Dictionary<AccountType, Account> Accounts { get; }
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+        Accounts = new Dictionary<AccountType, Account>();
+    }
+
+    public bool AddAccount(AccountType type)
+    {
+        if (Accounts.ContainsKey(type))
+        {
+            return false;
+        }
+        Accounts[type] = new Account(type);
+        return true;
+    }
+
+    public Account GetAccount(AccountType type)
+    {
+        if (!Accounts.ContainsKey(type))
+        {
+            return Accounts[type];
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
