c11de98989719b9fbeecd3b9d67b581f1027b039
Ayman Yaqoub-Okponobi <aymanokponobi13@gmail.com>
2025-06-20 11:49:39 +0100

    Add files via upload

---
+++ BankingApp/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Account
+{
+    public AccountType Type { get; }
+    public decimal Balance { get; private set; }
+
+    public Account(AccountType type)
+    {
+        Type = type;
+        Balance = 0;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            return false;
+        }
+        Balance -= amount;
+        return true;
+    }
+
+    public bool Transfer(Account target, decimal amount)
+    {
+        if (Withdraw(amount))
+        {
+            target.Deposit(amount);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ BankingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum AccountType { 
+    Savings, 
+    Current 
+}
+
+    
+class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new BankSystem();
+        app.Start();
+    }
+}
+
+// Hello new world
+
+
